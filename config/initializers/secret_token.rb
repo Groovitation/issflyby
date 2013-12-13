ca4bf9211df72d46eb5273a431b443a5196d6d9c
Ben Nelson <nelson.blaha@gmail.com>
2013-12-13 14:04:10 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Issflyby::Application.config.secret_key_base = 'c30e2530c9ec6766b1eba48a790bd4eaac79d8d200c69c8ca08df2c35114bb6a96b26dbcba70cf347dd2e06e051444d9d10c4069408c36a9cc2191d4e748feed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Issflyby::Application.config.session_store :cookie_store, key: '_issflyby_session'
