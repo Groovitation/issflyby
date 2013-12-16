4318342f18e72abce76cb2e99372e01aa808a593
Ben Nelson <nelson.blaha@gmail.com>
2013-12-15 22:26:36 -0600

    Add complex card management TODOs to pass.rb for the future

--- app/models/pass.rb
+++ app/models/pass.rb
@@ -7,6 +7,7 @@ class Pass < ActiveRecord::Base
 	def schedule_notifications
 		#TODO schedule a NotificationJob for an advance_notify x minutes prior to the pass, to give users time to organize
 		#TODO schedule a NotificationJob for passing_notify to tell users the object should be visible now
+		#TODO schedule a job for deleting the cards when the pass is complete. It will be necessary to store the card's ID returned in reply to the card's mirror API post
 	end
 
 	def weather_permitting
@@ -17,6 +18,7 @@ class Pass < ActiveRecord::Base
 	def passing_notify
 		if weather_permitting
 			self.user.send_glass_card({text:self.spacecraft.name+" is passing over right now!",isBundleCover:true})
+			# TODO delete advance_notify card
 			self.spacecraft.spacepeople.each do |sp|
 				self.user.send_glass_card({text:sp.name+" is on board",isBundleCover:false},false)
 			end
