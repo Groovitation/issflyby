a84b7f6f2308b702d984c939ba2dab205cb18507
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-12-15 21:45:59 -0600

    Adds spacepeople update

--- app/models/spaceperson.rb
+++ app/models/spaceperson.rb
@@ -1,3 +1,11 @@
 class Spaceperson < ActiveRecord::Base
   belongs_to :Spacecraft
+  attr_accessible :name, :craft
+  def self.update_all
+  	people = HTTParty.get('http://api.open-notify.org/astros.json')
+    puts people
+    people['people'].map do |person|
+		spaceperson = Spaceperson.find_or_create_by(name:person['name'], craft:person['craft'])
+    end
+  end
 end
