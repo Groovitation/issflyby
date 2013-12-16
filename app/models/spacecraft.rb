10d15631a01174bc663f5af3a0fe4546ab40d3ee
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-12-15 21:23:05 -0600

    Adds iss update

--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -1,11 +1,10 @@
 class Spacecraft < ActiveRecord::Base
   attr_accessible :name, :lat, :long
   validates_presence_of :name
-  def self.locate_iss
-    location = HTTParty.get('http://api.open-notify.org/iss-now.json')
+  
+  def self.update_iss_location
+  	location = HTTParty.get('http://api.open-notify.org/iss-now.json')
     puts location
-  end
-  def update_iss_location
     iss = Spacecraft.find_or_create_by(name: 'ISS')
     iss.update(lat:location['iss_position']['latitude'], long: location['iss_position']['latitude'])
   end
