f9720d4814895f7ce333567df6e9bb896055c9f6
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-12-15 20:30:34 -0600

    Adds location to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216013727) do
+ActiveRecord::Schema.define(version: 20131216023004) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20131216013727) do
     t.datetime "updated_at"
     t.string   "access_token"
     t.string   "refresh_token"
+    t.float    "lat"
+    t.float    "long"
   end
 
 end
