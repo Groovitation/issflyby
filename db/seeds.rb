da0aca3373fcd27c284e22752408802c8a68cc9d
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-01-11 19:11:01 -0600

    Adds Spacecraft.update_iss to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+
+Spacecraft.update_iss_location 
