3388bdfc4b90f89419a8684d51fe2655ad66bcab
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-01-19 19:22:08 -0600

    Begins filling cucumber steps

--- features/step_definitions/user_panel_steps.rb
+++ features/step_definitions/user_panel_steps.rb
@@ -1,35 +1,39 @@
 Given(/^I am an authenticated google user$/) do
   pending # express the regexp above with the code you wish you had
+  #User.create(name: "Mashallah Behbehani", email: , provider: , uid: , access_token: ENV['test_access_token'] , refresh_token: ENV['test_access_token'])
 end
 
 Given(/^I have the following upcoming flybys:$/) do |table|
   # table is a Cucumber::Ast::Table
   pending # express the regexp above with the code you wish you had
+  
 end
 
 When(/^I go to the homepage$/) do
-  pending # express the regexp above with the code you wish you had
+  visit "/"
 end
 
 When(/^I click "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  click_link(arg1)
 end
 
 Then(/^the page shows:$/) do |table|
   # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  table.hashes.each do |flyby|
+    page.has_content?(flyby.to_s)
+  end
 end
 
 Then(/^I am redirected to the landing page$/) do
-  pending # express the regexp above with the code you wish you had
+  page.has_content?('ISS Flyby!')
 end
 
 Then(/^I see the message "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  page.has_content?(arg1)
 end
 
 When(/^I see the page has reloaded, and shows "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  page.has_content?(arg1)
 end
 
 Then(/^I am sent a card saying "(.*?)"$/) do |arg1|
