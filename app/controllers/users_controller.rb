6a30efdb6210063c7302695f79b447c8e907bc09
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-01-26 15:03:43 -0600

    Sets card to central time temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def demo_card
-    if current_user.send_glass_card({text:"International Space Station is passing over soon! "+(Time.now.utc+7.minutes).to_s,isBundleCover:true})
+  	flyby_time = Time.now.in_time_zone("Central Time (US & Canada)")+7.minutes
+    if current_user.send_glass_card({text:"International Space Station flyby coming up! "+ flyby_time.strftime("%I:%M %p") ,isBundleCover:true})
       redirect_to root_url, notice: "Demo card sent to glass."
     else
       redirect_to root_url, notice: "Demo card could not be sent!"
