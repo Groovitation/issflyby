4a833446d0098d132de482d613fc235c10e89159
Ben Nelson <nelson.blaha@gmail.com>
2014-01-26 22:01:32 -0600

    Use UTC for demo card risetime calculation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def demo_card
-  	flyby_time = Time.now.in_time_zone("Central Time (US & Canada)")+7.minutes
 	  unless iss = Spacecraft.where(apiname:"ISS").first
       iss = Spacecraft.create(name:"International Space Station", endpoint:"http://api.open-notify.org/iss-pass.json", apiname:"ISS")
     end
-    pass = Pass.create(user_id:current_user.id,spacecraft_id:iss.id,risetime:flyby_time.strftime(" at %I:%M %p CST!"),demo:true)
+    pass = Pass.create(user_id:current_user.id,spacecraft_id:iss.id,risetime:Time.now.utc+7.minutes,demo:true)
     pass.advance_notify
     redirect_to root_url, notice: "Demo card sent to glass."
   end
