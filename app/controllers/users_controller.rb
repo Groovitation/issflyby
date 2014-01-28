644df5fefdcd91ce71409ae1151bb8a837f6e824
Ben Nelson <nelson.blaha@gmail.com>
2014-01-27 19:46:00 -0600

    Improved clarity in demo card controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def demo_card
-	  unless iss = Spacecraft.where(apiname:"ISS").first
+    iss = Spacecraft.where(apiname:"ISS").first
+    unless iss
       iss = Spacecraft.create(name:"International Space Station", endpoint:"http://api.open-notify.org/iss-pass.json", apiname:"ISS")
     end
+    
     pass = Pass.create(user_id:current_user.id,spacecraft_id:iss.id,risetime:Time.now.utc+7.minutes,demo:true)
     pass.advance_notify
     redirect_to root_url, notice: "Demo card sent to glass."
