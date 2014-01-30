66bd8569ec45f3e72b1fa4841c527b502dddd6b0
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-01-29 23:23:09 -0600

    Adds transition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,12 @@
     $( document ).ready(function() {
         $('#iss').animate({"top": "30%",  "right": "110%"}, 30000, "linear");
         $('.slider').glide({
-            autoplay: 3000,
+            autoplay: 3000000,
             nav: false,
             arrowLeftText : "<",
-            arrowRightText: ">"
+            arrowRightText: ">",
+            // beforeTransition:$('.description>p').fadeOut( 200, function() {}),
+            afterTransition: $('.description>p').fadeIn( 2000, function() {})
         });
     });
 
