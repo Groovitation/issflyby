7226fb08030ecc18fb7b5a7c6ac6bee7b4e26f81
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-02-06 20:32:06 -0600

    Creates sign out route and link

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Issflyby::Application.routes.draw do
 	root :to => 'application#index'
 	get '/auth/:provider/callback', to: 'sessions#create'
 	get '/sign_in', to: 'sessions#new', as: 'sign_in'
+	get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
   get '/demo_card', to: 'users#demo_card', as: 'demo_card'
 end
