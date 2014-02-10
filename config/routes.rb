e46dba693625e50d76050219a12c0f97e6a46797
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-02-09 18:56:26 -0600

    sets up press page routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Issflyby::Application.routes.draw do
 	get '/auth/:provider/callback', to: 'sessions#create'
 	get '/sign_in', to: 'sessions#new', as: 'sign_in'
 	get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
+	get '/press', to: 'pages#press', as: 'press'
   get '/demo_card', to: 'users#demo_card', as: 'demo_card'
 end
