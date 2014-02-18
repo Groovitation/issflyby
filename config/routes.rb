b6bcc56f7d628cf27232cc3c7af81dda27dbc09d
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-02-17 18:35:45 -0600

    Adds privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Issflyby::Application.routes.draw do
 	root :to => 'application#index'
 	get '/auth/:provider/callback', to: 'sessions#create'
 	get '/sign_in', to: 'sessions#new', as: 'sign_in'
+	get '/privacy', to: 'pages#privacy', as: 'privacy'
 	get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
 	get '/press', to: 'pages#press', as: 'press'
   get '/demo_card', to: 'users#demo_card', as: 'demo_card'
