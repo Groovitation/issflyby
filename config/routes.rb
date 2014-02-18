f39282e7cff362926e65c60178ed6ecfe33352ff
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-02-17 18:44:51 -0600

    Adds terms page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Issflyby::Application.routes.draw do
 	get '/auth/:provider/callback', to: 'sessions#create'
 	get '/sign_in', to: 'sessions#new', as: 'sign_in'
 	get '/privacy', to: 'pages#privacy', as: 'privacy'
+	get '/terms', to: 'pages#terms', as: 'terms'
 	get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
 	get '/press', to: 'pages#press', as: 'press'
   get '/demo_card', to: 'users#demo_card', as: 'demo_card'
