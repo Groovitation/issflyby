0b7fd49205cc978162bb2f8214fd6fbd31807dd7
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-06-28 11:51:07 -0500

    Disables remote card deletion

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,9 +2,9 @@ class Card < ActiveRecord::Base
   attr_accessible :mirror_id, :pass_id
   belongs_to :pass
 
-  before_destroy :remove_from_timeline
-
-  def remove_from_timeline
-    HTTParty.delete('https://www.googleapis.com/mirror/v1/timeline/'+self.mirror_id.to_s, headers: { 'Content-Type' => 'application/json', 'Authorization' => 'Bearer '+self.pass.user.access_token })
-  end
+  # before_destroy :remove_from_timeline
+  #
+  # def remove_from_timeline
+  #   HTTParty.delete('https://www.googleapis.com/mirror/v1/timeline/'+self.mirror_id.to_s, headers: { 'Content-Type' => 'application/json', 'Authorization' => 'Bearer '+self.pass.user.access_token })
+  # end
 end
